ed3fcf7177ff225de63ef01fcec47e32ce318ea7
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-02-18 12:27:48 +0200

    Feat: about page

---
+++ src/pages/About/About.js
@@ -0,0 +1,22 @@
+import "./About.css"
+const About = () => {
+    return (
+
+        <div class="about">
+            <h1 class="title">ABOUT ME</h1>
+            <div class="para-text">
+                <p>I'm Bakhos Ibrahim-Rachwan I graduated from University of Balamand with B.Eng. in Mechanical Engineering.
+                Refusing to wait helplessly for a job oppurtunity during the Covid-19 pandemic.
+                    </p>
+                <p>I joined an immersive bootcamp provided by Re:Coded. During the bootcamp I learned technologies like Javascript, CSS, HTML and ReactJS.
+                Along with completeing a Web Development course with Flatiron School.
+                    </p>
+                <p>I'm constantly learning new things. currently those things include gaining more experience with React Native and Python</p>
+                <p>Currently seeking an oppotunity in the tech industry.
+                        Frequently praised as quality-oriented by my peers. I can be relied upon to help your company achieve its goals.</p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
