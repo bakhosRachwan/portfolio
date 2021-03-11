a73e1e6051c586755ccb2dc90607dbd5be92ad24
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-03-11 23:43:23 +0200

    Add: add data to projects

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import "./Card.css"
 import { AiFillGithub } from 'react-icons/ai'
+import { FiLink } from 'react-icons/fi'
 
 const Card = ({ project }) => {
     return (
@@ -9,6 +10,7 @@ const Card = ({ project }) => {
                 <h5 className="card-title">{project.name}</h5>
                 <p className="card-text">{project.description}</p>
                 <a href={project.link} target="_blank" ><AiFillGithub size="2rem" /></a>
+                <a href={project.demo} target="_blank" ><FiLink size="2rem" /></a>
             </div>
         </div>
     );
--- src/pages/Projects/data.js
+++ src/pages/Projects/data.js
@@ -1,23 +1,27 @@
 import movieImg from "./movies.png";
 import portfolioImg from "./portfolio.png";
+import todoImg from "./todo.png";
 
 export const projects =[
     {
         name: "Portfolio",
         img: portfolioImg,
         description: "My personal website the one you are in right now feel free to check the code on github.",
-        link: "https://github.com/bakhosRachwan/portfolio"
+        link: "https://github.com/bakhosRachwan/portfolio",
+        demo: "https://bakhos-rachwan.netlify.app/"
     },
     {
         name: "Movie App",
         img: movieImg,
         description: "An App that fetches movies from TMDB API and you can search by name and genre. Each movie lists rating, trailer and actress information.",
-        link: "https://github.com/bakhosRachwan/movie-app-react-js"
+        link: "https://github.com/bakhosRachwan/movie-app-react-js",
+        demo: "https://mjbs-movie-app.netlify.app/"
     },
     {
         name: "Todo List",
-        img: movieImg,
+        img: todoImg,
         description: "Todo list app that manages tasks.",
-        link: "https://github.com/bakhosRachwan/todo-list"
+        link: "https://github.com/bakhosRachwan/todo-list",
+        demo: "https://ecstatic-todo.netlify.app/"
     }
 ]
