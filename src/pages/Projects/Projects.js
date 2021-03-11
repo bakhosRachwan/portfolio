fbfc0fc5ae0002f5aea6588dda7c808507aa0b82
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-03-11 22:18:56 +0200

    Style: add to projects data

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,23 +1,8 @@
 import "./Projects.css"
 import Card from "../../components/Card/Card";
-import movieImg from "./movies.png";
-import portfolioImg from "./portfolio.png";
+import { projects } from "./data";
 
 const Projects = () => {
-    const projects =[
-        {
-            name: "Portfolio",
-            img: portfolioImg,
-            description: "My personal website the one you are in right now feel free to check the code on github.",
-            link: "https://github.com/bakhosRachwan/portfolio"
-        },
-        {
-            name: "Movie App",
-            img: movieImg,
-            description: "An App that fetches movies from TMDB API and you can search by name and genre. Each movie lists rating, trailer and actress information.",
-            link: "https://github.com/bakhosRachwan/movie-app-react-js"
-        }
-    ]
     return ( 
         <div className="projects bg-dark" id="projects">
             <h1 className="title mt-5"> Projects</h1>
---
+++ src/pages/Projects/data.js
@@ -0,0 +1,23 @@
+import movieImg from "./movies.png";
+import portfolioImg from "./portfolio.png";
+
+export const projects =[
+    {
+        name: "Portfolio",
+        img: portfolioImg,
+        description: "My personal website the one you are in right now feel free to check the code on github.",
+        link: "https://github.com/bakhosRachwan/portfolio"
+    },
+    {
+        name: "Movie App",
+        img: movieImg,
+        description: "An App that fetches movies from TMDB API and you can search by name and genre. Each movie lists rating, trailer and actress information.",
+        link: "https://github.com/bakhosRachwan/movie-app-react-js"
+    },
+    {
+        name: "Todo List",
+        img: movieImg,
+        description: "Todo list app that manages tasks.",
+        link: "https://github.com/bakhosRachwan/todo-list"
+    }
+]
