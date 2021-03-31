27da4f1d0b057d24f942a892a66038f26c501c00
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-01 00:06:34 +0300

    Add: add new project

--- src/pages/Projects/data.js
+++ src/pages/Projects/data.js
@@ -1,6 +1,7 @@
 import movieImg from "./movies.png";
 import portfolioImg from "./portfolio.png";
 import todoImg from "./todo.png";
+import quizImg from "./quiz-1.jpg";
 
 export const projects =[
     {
@@ -26,5 +27,21 @@ export const projects =[
         description: "Todo list app that manages tasks.",
         link: "https://github.com/bakhosRachwan/todo-list",
         demo: "https://ecstatic-todo.netlify.app/"
+    },
+    {
+        id: 4,
+        name: "Trivia quiz app",
+        img: quizImg,
+        description: "Like the name implies its a questions game. the app fetches questions from API and the user can choose from different settings.",
+        link: "https://github.com/bakhosRachwan/quiz-game",
+        demo: "https://trivia-quiz-night-app.netlify.app/"
+    },
+    {
+        id: 5,
+        name: "Wallet Manager",
+        img: "https://img.freepik.com/free-vector/coming-soon-message-illuminated-with-light-projector_1284-3622.jpg?size=338&ext=jpg",
+        description: "Your electronic Wallet thet keep track of all you Income and Expenses.",
+        link: "https://github.com/bakhosRachwan/the-wallet-manager",
+        demo: ""
     }
 ]
