b0fc4f36fa1a8f4596d490935940a5f00dc01d7c
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-05-01 23:13:48 +0300

    Fix: fix link

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,4 +1,4 @@
-import "./Skills.css"
+import "./Skills.css";
 import html from "./skillsSvg/html-logo.svg";
 import css from "./skillsSvg/css-logo.svg";
 import javascript from "./skillsSvg/javascript-logo.svg";
@@ -12,49 +12,55 @@ import bootstrap from "./skillsSvg/bootstrap-logo.svg";
 import tailwindcss from "./skillsSvg/tailwindcss-logo.svg";
 import chakraui from "./skillsSvg/chakra-ui-logo.svg";
 const Skills = () => {
-    return ( 
-        <div className="skills bg-light" id="skills">
-            <h1>Experienced in</h1>
-            <div className="technologies">
-                <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
-                    <img src={html} alt="logo" />
-                </a>
-                <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-                <img src={css} alt="logo" />
-                </a>
-                <a href="https://www.javascript.com/">
-                <img src={javascript}alt="logo" />
-                </a>
-                <a href="https://reactjs.org/">
-                <img src={react} alt="logo" />
-                </a>
-                <a href="https://getbootstrap.com/">
-                <img src={bootstrap} alt="logo" />
-                </a>
-                <a href="https://tailwindcss.com/">
-                <img src={tailwindcss} alt="logo" />
-                </a>
-                <a href="https://chakra-ui.com/">
-                <img src={chakraui} alt="logo" />
-                </a>
-                <a href="https://git-scm.com/">
-                <img src={git} alt="logo" />
-                </a>
-                <a href="https://github.com/">
-                <img src={github}alt="logo"  />
-                </a>
-                <a href="https://www.figma.com">
-                <img src={figma} alt="logo" />
-                </a>
-                <a href="https://www.adobe.com/products/xd.html">
-                <img src={adobe} alt="logo" />
-                </a>
-                <a href="firebase.google.com/">
-                <img src={firebase} alt="logo" />
-                </a>
-            </div>
-        </div>
-     );
-}
- 
+  return (
+    <div className="skills bg-light" id="skills">
+      <h1>Experienced in</h1>
+      <div className="technologies">
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+          target="blank"
+        >
+          <img src={html} alt="logo" />
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+          target="blank"
+        >
+          <img src={css} alt="logo" />
+        </a>
+        <a href="https://www.javascript.com/" target="blank">
+          <img src={javascript} alt="logo" />
+        </a>
+        <a href="https://reactjs.org/" target="blank">
+          <img src={react} alt="logo" />
+        </a>
+        <a href="https://getbootstrap.com/" target="blank">
+          <img src={bootstrap} alt="logo" />
+        </a>
+        <a href="https://tailwindcss.com/" target="blank">
+          <img src={tailwindcss} alt="logo" />
+        </a>
+        <a href="https://chakra-ui.com/" target="blank">
+          <img src={chakraui} alt="logo" />
+        </a>
+        <a href="https://git-scm.com/" target="blank">
+          <img src={git} alt="logo" />
+        </a>
+        <a href="https://github.com/" target="blank">
+          <img src={github} alt="logo" />
+        </a>
+        <a href="https://www.figma.com" target="blank">
+          <img src={figma} alt="logo" />
+        </a>
+        <a href="https://www.adobe.com/products/xd.html" target="blank">
+          <img src={adobe} alt="logo" />
+        </a>
+        <a href="https://firebase.google.com/" target="blank">
+          <img src={firebase} alt="logo" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export default Skills;
