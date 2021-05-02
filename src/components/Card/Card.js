4cdca15560a5283bd23a598d6db2c36895fdbe24
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-05-02 17:22:57 +0300

    Style: modify card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,31 @@
-import "./Card.css"
-import { AiFillGithub } from 'react-icons/ai'
-import { FiLink } from 'react-icons/fi'
+import "./Card.css";
+import { AiFillGithub } from "react-icons/ai";
+import { FiLink } from "react-icons/fi";
 
 const Card = ({ project }) => {
-    return (
-        <div className="card ">
-            <img className="card-img-top" src={project.img} alt={project.name} />
-            <div className="card-body">
-                <h5 className="card-title">{project.name}</h5>
-                <p className="card-text">{project.description}</p>
-                <a href={project.link} target="_blank" ><AiFillGithub size="2rem" /></a>
-                <a href={project.demo} target="_blank" ><FiLink size="2rem" /></a>
-            </div>
+  return (
+        <div className="card profile-card">
+          <div className="card-img-block">
+            <img
+              className="card-img-top"
+              src={project.img}
+              alt={project.name}
+            />
+          </div>
+          <div className="card-body pt-0">
+            <h5 className="card-title">{project.name}</h5>
+            <p className="card-text">
+            {project.description}
+            </p>
+            <a href={project.link} target="blank">
+            <AiFillGithub size="2rem" />
+          </a>
+          <a href={project.demo} target="blank">
+            <FiLink size="2rem" />
+          </a>
+          </div>
         </div>
-    );
-}
+  );
+};
 
 export default Card;
