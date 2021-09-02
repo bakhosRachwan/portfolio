f56ea68cb2474988bdc27bd58bda2822b567e6bf
bakhosRachwan <bakhos.rachwan@gmail.com>
2021-09-02 22:11:07 +0300

    .

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,37 +4,40 @@ const ContactForm = () => {
   return (
     <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" className="forms">
       <input type="hidden" name="form-name" value="contact"></input>
-      <div class="form-group">
+      <div className="form-group">
         <label for="fullname">Full Name</label>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="fullname"
           placeholder="Enter Full Name"
           required
         />
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="exampleFormControlInput1">Email address</label>
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="name@example.com"
           required
           />
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="exampleFormControlTextarea1">Write your Message</label>
         <textarea
-          class="form-control"
+          className="form-control"
           id="exampleFormControlTextarea1"
           placeholder="Type here..."
           rows="3"
           required
           ></textarea>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <div className="form-group">
+      <div data-netlify-recaptcha="true"></div>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   );
 };
