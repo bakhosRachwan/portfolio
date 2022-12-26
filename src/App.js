b90e9f735326e9901109e85e3bdc8811031c3237
bakhosRachwan <bakhos.rachwan@gmail.com>
2022-12-26 18:56:28 +0200

    Build: Experience Page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
+import Experience from "./pages/Experience/Experience";
 import Projects from "./pages/Projects/Projects";
 import Skills from "./pages/Skills/Skills";
+import Contact from "./pages/Contact/Contact";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -15,6 +16,7 @@ function App() {
         <Navbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/experience" component={Experience} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/skills" component={Skills} />
         <Route exact path="/contact" component={Contact} />
---
+++ src/pages/Experience/Experience.js
@@ -0,0 +1,13 @@
+import "./Experience.css"
+const Experience = () => {
+    return ( 
+        <div className="experience" id="experience">
+            <h1 className="title">EXPERIENCE</h1>
+            <div className="para-text">
+            Under Construction...
+            </div>
+        </div>
+     );
+}
+ 
+export default Experience;
